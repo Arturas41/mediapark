d5264aa816ca262d11b453b407d56500b670e19b
arturaszilinskas41 <arturaszilinskas41@gmail.com>
2020-11-24 19:38:25 +0200

    test if logged in users has access

--- tests/Controller/MainControllerTest.php
+++ tests/Controller/MainControllerTest.php
@@ -3,9 +3,11 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Repository\UserRepository;
 
 class MainControllerTest extends WebTestCase
 {
+
     public function testIfRedirects()
     {
         $client = static::createClient();
@@ -15,5 +17,19 @@ class MainControllerTest extends WebTestCase
         $this->assertEquals(302, $client->getResponse()->getStatusCode());
 
         $this->assertResponseRedirects('/login');
+    }
+
+    public function testIfLoggedInUsersHasAccess()
+    {
+        $client = static::createClient();
+
+        $userRepository = static::$container->get(UserRepository::class);
+        $testUser = $userRepository->findOneBy(['email' => 'asdf@asdf.asdf']);
+
+        $client->loginUser($testUser);
+
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
     }
 }
