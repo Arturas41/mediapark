a12e72069a9b7c9935e13b1c631ff95d20d12478
arturaszilinskas41 <arturaszilinskas41@gmail.com>
2020-11-24 22:09:08 +0200

    HolidayApisFixtures

---
+++ src/DataFixtures/HolidayApisFixtures.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\HolidayApis;
+
+class HolidayApisFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $holidayApis = new HolidayApis();
+
+        $holidayApis->setName('kayaposoft');
+        $holidayApis->setUrl('https://kayaposoft.com/enrico/json/v2.0/');
+        $manager->persist($holidayApis);
+
+        $manager->flush();
+    }
+}
