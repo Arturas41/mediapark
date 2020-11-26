e1f2770c3e6d2952e96122fd6870f58ab953fec7
arturaszilinskas41 <arturaszilinskas41@gmail.com>
2020-11-27 01:59:13 +0200

    supportedcountries form

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,21 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Country;
+use App\Entity\SupportedCountry;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\Type\SupportedCountryType;
+
 
 class MainController extends AbstractController
 {
+    protected EntityManager $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
      /**
       * @Route("/", name="main_show")
       */
     public function show(): Response
     {
-        $tests = ['test1', 'test2', 'test3'];
+//        $country = $this->em->getRepository(Country::class)->findOneBy(
+//            ['name' => 'Denmark']
+//        );
 
-        return $this->render('main/layout.html.twig', [
-            'tests' => $tests
+        $supportedCountry = new SupportedCountry();
+//        $supportedCountry->setCountry($country);
+        $supportedCountry->setToDate(new \DateTime('tomorrow'));
+        $supportedCountry->setFromDate(new \DateTime('tomorrow'));
+
+        $form = $this->createForm(SupportedCountryType::class, $supportedCountry);
+
+        return $this->render('main/index.html.twig',[
+            'form' => $form->createView(),
         ]);
     }
 }
